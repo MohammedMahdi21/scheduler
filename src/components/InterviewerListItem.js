3b0e30b34eb4c65831f8259f1ae776099f2b64c5
Mohammed Hussein <mmd@Mohammeds-MBP-2.cogeco.local>
2022-07-18 22:09:42 -0400

    build InterviewerList component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,7 +5,7 @@ import classNames from "classnames";
 export default function InterviewerListItem(props) {
 
   const interviewerClass = classNames("interviewers__item",
-  {"interviewers__item--selected": props.selected})
+    { "interviewers__item--selected": props.selected })
 
   return (
     <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
