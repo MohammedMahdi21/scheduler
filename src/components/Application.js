31fec6fd46cec35ee0ffdf14c765412bbc8330fa
Mohammed Hussein <mmd@Mohammeds-MBP-2.cogeco.local>
2022-07-22 23:04:30 -0400

    add <Appointment> views to our application in webpack-dev-server environment.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-
+import Appointment from "./Appointment";
 import "components/Application.scss";
 import DayList from "./DayList";
 
 export default function Application(props) {
 
-const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState("Monday")
 
   const days = [
     {
@@ -24,6 +24,56 @@ const [day, setDay] = useState("Monday")
       spots: 0,
     },
   ];
+
+
+  const appointments = {
+    "1": {
+      id: 1,
+      time: "12pm",
+    },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+    },
+    "4": {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer: {
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        }
+      }
+    },
+    "5": {
+      id: 5,
+      time: "4pm",
+    }
+  };
+
+
+  const appointmentsList = Object.values(appointments).map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    )
+  })
 
   return (
     <main className="layout">
@@ -49,7 +99,13 @@ const [day, setDay] = useState("Monday")
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsList}
+        <Appointment
+          key="last"
+          time="5pm"
+        />
       </section>
     </main>
-  );
-}
+  )
+};
+
