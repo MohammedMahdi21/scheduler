b0ceb112e0aa98a17835c8a14c9fb764e65e7ee8
Mohammed Hussein <mmd@Mohammeds-MBP-2.cogeco.local>
2022-07-28 23:08:00 -0400

    build custom hook to update the mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, -1), mode])
+    } else {
+      setHistory(prev => [...prev, mode]);
+    }
+  }
+
+  function back() {
+    if (history.length <= 1) {
+      return;
+    }
+
+    setHistory((prev) => [...prev.slice(0, -1)]);
+  }
+
+  const mode = history[history.length - 1];
+
+  return { mode, transition, back };
+}
