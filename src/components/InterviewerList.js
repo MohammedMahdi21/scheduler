742719a670bb05dad84b306735b76236e56e3814
Mohammed Hussein <mmd@Mohammeds-MBP-2.cogeco.local>
2022-08-03 21:12:09 -0400

    add a test to make sure interviwers is an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 export default function InterviewerList(props) {
 
   
@@ -23,3 +24,6 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
